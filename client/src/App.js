d4198f4cd11cce3f7a6caa897129e384928c6325
Carlos Acevedo <charlyacevedo@hotmail.com>
2022-05-21 15:06:04 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logoFittNet from "../../logo_fitnet.jpg"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FittNet</h1>
+      <img height="200" src={logoFittNet} />  
+    </div>
+  );
+}
+
+export default App;
